22e1026be64e51990c99c0ceecde6653a92c5800
beastmode900 <mjbaker3931@gmail.com>
2024-07-23 10:40:54 -0400

    progress towards traversal, figuring out branches lolll

---
+++ scripts/navigation.js
@@ -0,0 +1,20 @@
+
+//clickable elements 
+const clickables = {
+    left: "",
+    forward: "",
+    right: "",
+
+}
+
+//different locations
+const locations = {
+    homePage: {
+        left: 1,
+        right: 1,
+        forward: 1,
+    }
+}
+
+const clickableElements = document.querySelectorAll(".clickable");
+
